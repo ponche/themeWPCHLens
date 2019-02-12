e1ead901f34ec8261d80ad430aad30e134de5b78
Thery <j.thery2311@outlook.fr>
2019-02-12 17:56:08 +0100

    add amenagement and licence.txt

---
+++ jeremy_chlens/js/main.js
@@ -0,0 +1,10 @@
+$('.Show').click(function() {
+    $('#target').show(800);
+    $('.Show').hide(0);
+    $('.Hide').show(0);
+  });
+  $('.Hide').click(function() {
+    $('#target').hide(800);
+    $('.Show').show(0);
+    $('.Hide').hide(0);
+  });
