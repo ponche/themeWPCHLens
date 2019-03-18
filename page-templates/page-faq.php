c6996c9f80b56cd419cc481cd1d2ae243ba2efc8
Rémi <ponche62880@mail.com>
2019-03-18 13:42:31 +0100

    add page FAQ

---
+++ page-templates/page-faq.php
@@ -0,0 +1,202 @@
+<?php
+
+/*
+
+Template Name: Page FAQ
+
+*/
+	get_header();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/bootstrap_custom.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="faq.js" defer></script>
+    <title>Nouvel hôpital de Lens - FAQ</title>
+</head>
+<body>
+    
+
+
+<section class="container sorting">
+    <div class="col-lg-12">
+
+    <div class="row justify-content-around">
+        <div class="col-lg-12">
+            <h1 class="faq-title">FAQ</h1>
+            <p class="faq-text">Retrouvez les questions fréquemment posées et leurs réponses.</p>
+
+       
+            <nav class="tags-nav faq">
+              <a href="#" class="tag-link filter">Patrimoine</a> <a href="#" class="tag-link filter">Délais</a> <a href="#" class="tag-link">Financement</a> <a href="#" class="tag-link">Perturbations</a> <a href="#" class="tag-link">Accessibilité</a> <a href="#" class="tag-link">Services</a>
+            </nav>
+
+            
+        </div>
+    </div>
+
+    <div class="row justify-content-around">
+        <div class="col-lg-12">
+        
+            <div class="cards">
+
+                <div class=" card [ is-collapsed ] ">
+                    <div class="card__inner [ js-expander ]">
+                      <span>Qu’est-ce que ça va coûter aux habitants ?</span>
+                    </div>
+                    <div class="card__expander">
+                      <i class="fa fa-times [ js-collapser ]"></i>
+                      Non. Le Nouvel Hôpital de Lens sera financé par l’Etat (102M€) et par l’hôpital via des emprunts (178M€). Les collectivités locales (département et CALL) ne prendront en charge qu’une partie des voies d’accès, charges qui sont intégrées dans les budgets actuels.
+                    </div>
+                  </div>
+              
+                  <div class=" card [ is-collapsed ] ">
+                    <div class="card__inner [ js-expander ]">
+                      <span>Que va devenir le site actuel de l’hôpital ?</span>
+                    </div>
+                    <div class="card__expander">
+                      <i class="fa fa-times [ js-collapser ]"></i>
+                      Des discussions sont en cours avec les collectivités locales, notamment la CALL. Le Centre Hospitalier de Lens, une fois qu’il aura déménagé, n’aura plus aucune activité sur le site actuel. Nous n’avons donc aucun intérêt à conserver les bâtiments actuels. Pour les collectivités cela peut avoir un intérêt puisqu’il y a 11 ha en bordure de centre-ville.
+                    </div>
+                  </div>
+              
+                  <div class=" card [ is-collapsed ] ">
+                    <div class="card__inner [ js-expander ]">
+                      <span>Les urgences seront-elles plus grandes ?</span>
+                    </div>
+                    <div class="card__expander">
+                      <i class="fa fa-times [ js-collapser ]"></i>
+                      Les locaux actuels des urgences représentent une surface de 1637 m² SDO. Les urgences au sein du Nouvel Hôpital de Lens feront plus du double : 3770 m² SDO. Les surfaces, les équipements et le capacitaire ont été identifiés par les équipes des urgences et calées sur les ratios de l’ANAP (Agence Nationale d’Appui à la Performance) sur une prévision d’activité à 80 000 passages par an. Aujourd’hui, 70 000 patients passent aux urgences dans des locaux prévus pour en recevoir 35 000.
+                    </div>
+                  </div>
+              
+                  <div class=" card [ is-collapsed ] ">
+                    <div class="card__inner [ js-expander ]">
+                      <span>Quels seront les impacts sur la sortie Lens Ouest ?</span>
+                     </div>
+                    <div class="card__expander">
+                      <i class="fa fa-times [ js-collapser ]"></i>
+                      L’échangeur Lens Nord sera modifié pour permettre un accès direct au Nouvel Hôpital de Lens. Ce sera l’accès principal. La circulation devrait donc être plus fluide. Toutefois, en cas de blocage de la sortie Lens Ouest (accident par exemple), un accès B doit être prévu. La sortie Lens Ouest pourrait, dans ces cas exceptionnels, devenir un accès privilégié.
+                    </div>
+                  </div>
+              
+                  <div class=" card [ is-collapsed ] ">
+                    <div class="card__inner [ js-expander ]">
+                      <span>Y aura-t-il plus de parkings que le site actuel et seront-ils gratuits ?</span>
+                    </div>
+                    <div class="card__expander">
+                      <i class="fa fa-times [ js-collapser ]"></i>
+                      Il y a moins de 900 places de parkings sur le site actuel. Le Nouvel Hôpital en comptera 2000. Des parkings seront dédiés pour le personnel, d’autres pour les consultations ou pour les urgences.
+Les parkings seront gratuits. Si aujourd’hui Lille, Arras ou d’autres Centres Hospitaliers ont des parkings payants (en totalité ou en partie), c’est parce que leurs parkings sont soit trop petits, soit utilisés par des personnes qui ne viennent pas à l’hôpital. Ce ne sera pas le cas pour le Nouvel Hôpital.
+                    </div>
+                  </div>
+              
+                  <div class=" card [ is-collapsed ] ">
+                    <div class="card__inner [ js-expander ]">
+                      <span>Peut-on construire un hôpital plus grand ?</span>
+                    </div>
+                    <div class="card__expander">
+                      <i class="fa fa-times [ js-collapser ]"></i>
+                      Le CH de Lens a obtenu une autorisation de reconstruction par une instance interministérielle sur un projet de 71 740 m². Nous devons respecter cette demande et optimiser les surfaces, au risque sinon de voir annuler le projet.
+                    </div>
+                  </div>
+              
+                  <div class=" card [ is-collapsed ] ">
+                    <div class="card__inner [ js-expander ]">
+                      <span>La sortie Lens Nord va-t-elle être condamnée pendant les travaux ?</span>
+                    </div>
+                    <div class="card__expander">
+                      <i class="fa fa-times [ js-collapser ]"></i>
+                      Des perturbations seront à prévoir pendant les travaux. Mais les services du département, chargés des ronds point, ont l’habitude de ce type de chantier et feront tout pour limiter les désagréments. Aucune fermeture n’est à l’ordre du jour.
+                    </div>
+                  </div>
+              
+                  <div class=" card [ is-collapsed ] ">
+                    <div class="card__inner [ js-expander ]">
+                      <span>Y aura-t-il des retards liés à l’acquisition des terrains ?</span>
+                    </div>
+                    <div class="card__expander">
+                      <i class="fa fa-times [ js-collapser ]"></i>
+                      A l’heure actuelle 80% des terrains sont acquis ou sur le point de l’être. Les agriculteurs, propriétaires comme exploitants, ont bien compris les enjeux et ont fait preuve de responsabilité dans la gestion de ce dossier.
+                    </div>
+                  </div>
+              
+                  <div class=" card [ is-collapsed ] ">
+                    <div class="card__inner [ js-expander ]">
+                      <span>Le petit chemin de Lens sera-t-il coupé ?</span>
+                    </div>
+                    <div class="card__expander">
+                      <i class="fa fa-times [ js-collapser ]"></i>
+                      Les loosois qui ont l’habitude de passer par la rue Faidherbe puis par le petit chemin de Lens en passant sous l’A21 pourront toujours le faire. La route sera dévoyée et contournera le Nouvel Hôpital de Lens le long de la trame verte.
+                    </div>
+                  </div>
+          </div>
+
+    
+
+          <nav class="tags-nav faq col-lg-12">
+            <a href="#" class="tag-link">Download More</a> 
+         </nav>
+
+</section>  
+<section class="container-fluid faq"> 
+<div class="container form">      
+
+    <div class="row justify-content-around">
+
+            <div class="row justify-content-around">
+                    <div class="col-lg-4 text-center">
+
+                        <h3>D’autres questions ?</h3>
+                        <p>Faîtes part de vos remarques, poser une question, faire une demande d’éclaircissement ou vous exprimer simplement sur ce projet ?</p>
+                    </div>
+                    <form class="col-lg-6 form">
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Nom">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Prénom">
+                                </div>
+                            </div>
+                        </div>
+                    
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Adresse email">        
+                        </div>
+                                  
+                        <div class="form-group">
+                            <select id="inputState" class="form-control">
+                                <option selected>Choisissez un objet...</option>
+                                <option>Projet du nouvel hopital: Poser une question</option>
+                                <option>Projet du nouvel hopital: Donner son avis</option>
+                                <option>Contacter le service communication</option>
+                                <option>Déposer une candidature spontanée</option>
+                            </select>
+                        </div>
+        
+                        <div class="form-group">
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+        
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Envoyer</button>
+                        </div>
+                    </form>
+                </div>
+    </div>
+</section>
+
+<?php 
+get_footer() ; 
+
