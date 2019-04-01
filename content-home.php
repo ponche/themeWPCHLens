ee55405227c048df46ae4b853d317fc991027514
Rémi <ponche62880@mail.com>
2019-04-01 11:22:58 +0200

    add the new page for the home page

---
+++ content-home.php
@@ -0,0 +1,52 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-home-page">
+            <a href="">
+                <img src="<?= get_stylesheet_directory_uri(); ?>/img/item1.png" alt="">
+                <div class="item-hover">
+                    <p><strong>Visualiser</strong><br> le projet</p>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-home-page">
+            <a href="">
+                <img src="<?= get_stylesheet_directory_uri(); ?>/img/item2.png" alt="">
+                <div class="item-hover">
+                    <p><strong>Visualiser</strong><br> le projet</p>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-home-page">
+            <a href="">
+                <img src="<?= get_stylesheet_directory_uri(); ?>/img/item3.png" alt="">
+                <div class="item-hover">
+                    <p><strong>Visualiser</strong><br> le projet</p>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-home-page">
+            <a href="">
+                <img src="<?= get_stylesheet_directory_uri(); ?>/img/item4.png" alt="">
+                <div class="item-hover">
+                    <p><strong>Visualiser</strong><br> le projet</p>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-home-page">
+            <a href="">
+                <img src="<?= get_stylesheet_directory_uri(); ?>/img/item5.png" alt="">
+                <div class="item-hover">
+                    <p><strong>Visualiser</strong><br> le projet</p>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12 item-home-page">
+            <a href="">
+                <img src="<?= get_stylesheet_directory_uri(); ?>/img/item6.jpg" alt="">
+                <div class="item-hover">
+                    <p><strong>Visualiser</strong><br> le projet</p>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
