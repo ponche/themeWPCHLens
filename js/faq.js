81bb168b886ded61a639515a592e3ea4226cb5e0
Rémi <ponche62880@mail.com>
2019-04-02 16:11:26 +0200

    delete the $ symbol in the script javascript.
    it is not PHP

--- js/faq.js
+++ js/faq.js
@@ -2,36 +2,36 @@ jQuery(document).ready(function(){
 
 
 
-var $cell = jQuery('.card');
+var cell = jQuery('.card');
 
 //open and close card when clicked on card
-$cell.find('.js-expander').click(function() {
+cell.find('.js-expander').click(function() {
 
-  var $thisCell = jQuery(this).closest('.card');
+  var thisCell = jQuery(this).closest('.card');
 
-  if ($thisCell.hasClass('is-collapsed')) {
-    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
-    $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+  if (thisCell.hasClass('is-collapsed')) {
+    cell.not(thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+    thisCell.removeClass('is-collapsed').addClass('is-expanded');
     
-    if ($cell.not($thisCell).hasClass('is-inactive')) {
+    if (cell.not(thisCell).hasClass('is-inactive')) {
       //do nothing
     } else {
-      $cell.not($thisCell).addClass('is-inactive');
+      cell.not(thisCell).addClass('is-inactive');
     }
 
   } else {
-    $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-    $cell.not($thisCell).removeClass('is-inactive');
+    thisCell.removeClass('is-expanded').addClass('is-collapsed');
+    cell.not(thisCell).removeClass('is-inactive');
   }
 });
 
 //close card when click on cross
-$cell.find('.js-collapser').click(function() {
+cell.find('.js-collapser').click(function() {
 
-  var $thisCell = jQuery(this).closest('.card');
+  var thisCell = jQuery(this).closest('.card');
 
-  $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-  $cell.not($thisCell).removeClass('is-inactive');
+  thisCell.removeClass('is-expanded').addClass('is-collapsed');
+  cell.not(thisCell).removeClass('is-inactive');
 
 });
 
