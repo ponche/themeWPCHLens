9761ad548f83d966c4c5ce923fc413c955f062fb
Rémi <ponche62880@gmail.com>
2019-04-12 14:04:13 +0200

    add timeline in the projet page

---
+++ page-templates/page-projet.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+
+Template Name: Page Projet
+
+*/
+
+get_header() ; ?>
+
+<div class="col">
+
+    <?php echo do_shortcode('[th-slider]') ; ?>
+
+</div>
+
+<?php 
+get_footer() ; 
+
+
+
