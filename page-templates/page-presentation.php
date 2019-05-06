b5598c1a7899ab3903605718e1495d8b921d82fa
Rémi <ponche62880@gmail.com>
2019-05-06 10:29:31 +0200

    modif to loop wordpress with pods

--- page-templates/page-presentation.php
+++ page-templates/page-presentation.php
@@ -2,20 +2,17 @@
 
 /*
 
-Template Name: Page Présentation 
+Template Name: Page Presentation 
 
 */
 	get_header();
 ?>
 
-<!-- c'est moche, c'est juste pour le test -->
-<br><br><br> 
-
 <div class="col-12">
     <div class="container">
         <!-- contenu de la page --> 
         <div class="row">
-            <div class="col-12 col-lg-8">
+            <div class="col-12 col-lg-8 border border-primary">
                 <h2>Le titre </h2>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minima accusantium nihil illo facere odio dolore, doloremque ipsum eveniet inventore repudiandae officiis perspiciatis molestiae minus. Nisi iusto dolorem doloribus itaque.
@@ -52,6 +49,7 @@ Template Name: Page Présentation
                 <img src="#" alt="image de test">
             </div>
         </div>  
+
         <!-- la boucle pods --> 
         <?php
         $podPresentation = pods('presentation') ; 
@@ -67,16 +65,18 @@ Template Name: Page Présentation
             <p>
                 <?php echo $podPresentation->display('post_content') ; ?> 
             </p>
-        </div>
-        <div class="col-12 col-lg-4">
-            <?php echo $podPresentation->display('photo') ; ?>
+            </div>
+            <div class="col-12 col-lg-4">
+                <?php echo $podPresentation->display('photo') ; ?>
+            </div>
         </div>
 
         <!-- fin de la boucle pods --> 
         <?php
         }
         ?>
-    </div>
+
+    
     
 </div>
 
