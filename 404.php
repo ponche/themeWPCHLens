daee04ef3f271128a790f5f71aee22a38932dba7
Rémi <ponche62880@gmail.com>
2019-05-10 09:33:46 +0200

    delete search area in 404 page

--- 404.php
+++ 404.php
@@ -18,7 +18,6 @@ get_header(); ?>
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'sydney' ); ?></p>
 
-					<?php get_search_form(); ?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
